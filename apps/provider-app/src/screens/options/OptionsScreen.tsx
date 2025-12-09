bc5a80f3e9d8ffe256796877f5e1738b17c43ddf
Ehren Castillo <ehrencastillo@gmail.com>
2025-12-10 02:17:56 +0800

    feat(apps/provider-app): add sign out button

--- apps/provider-app/src/screens/options/OptionsScreen.tsx
+++ apps/provider-app/src/screens/options/OptionsScreen.tsx
@@ -1,10 +1,20 @@
+import { authClient } from '@lib/authClient';
 import { Button } from '@repo/components';
 import { colors } from '@repo/theme';
 import { FileClock, UserPen } from 'lucide-react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StyleSheet } from 'react-native';
+import { useLoading } from '@contexts/LoadingContext';
 
 export function OptionsScreen() {
+  const { setLoading } = useLoading();
+
+  const signOut = async () => {
+    setLoading(true);
+    await authClient.signOut();
+    setLoading(false);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Button title="Manage Profile" variant="outline" style={styles.button} leftIcon={
@@ -13,7 +23,7 @@ export function OptionsScreen() {
       <Button title="View Transaction History" variant="outline" style={styles.button} leftIcon={
         <FileClock size={24} color={colors.actionPrimary} />
       }/>
-      <Button title="Sign Out" variant="primary" />
+      <Button title="Sign Out" variant="primary" onPress={signOut} />
     </SafeAreaView>
   );
 }
