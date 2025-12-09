a48ae68f26d3b24515fbe3b7e94afb22fccdc7dd
Ehren Castillo <ehrencastillo@gmail.com>
2025-12-10 01:00:59 +0800

    feat(packages/components): add loading view component

---
+++ packages/components/src/LoadingView/LoadingView.tsx
@@ -0,0 +1,20 @@
+import { ActivityIndicator, StyleSheet } from "react-native";
+import { colors } from "@repo/theme";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+export function LoadingView() {
+  return (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size="large" color={colors.actionPrimary} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+});
