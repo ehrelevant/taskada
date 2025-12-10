af4a76d2a7304c88d962ef1dc8f6a4666755967f
Ehren Castillo <ehrencastillo@gmail.com>
2025-12-11 00:05:15 +0800

    chore(apps/provider-app): remove unnecessary logging

--- apps/provider-app/lib/helpers.ts
+++ apps/provider-app/lib/helpers.ts
@@ -2,8 +2,6 @@ import { API_URL } from "./env";
 import { authClient } from "./authClient";
 
 export async function apiFetch(endpoint: string, options?: RequestInit, authenticated = true) {
-  console.log('test');
-
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
     ...(options?.headers || {}),
