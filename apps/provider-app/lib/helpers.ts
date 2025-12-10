9f10bafa41bfe1adfac6b2e6326042db93d0a42b
Ehren Castillo <ehrencastillo@gmail.com>
2025-12-10 20:07:34 +0800

    feat(apps/provider): add custom api fetch function

---
+++ apps/provider-app/lib/helpers.ts
@@ -0,0 +1,31 @@
+import { API_URL } from "./env";
+import { authClient } from "./authClient";
+
+export async function apiFetch(endpoint: string, options?: RequestInit, authenticated = true) {
+  console.log('test');
+
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json',
+    ...(options?.headers || {}),
+  }
+
+  if (authenticated) {
+    // Add authentication cookie
+    const cookies = authClient.getCookie();
+    console.log(cookies);
+
+    return fetch(`${API_URL}${endpoint}`, {
+      ...options,
+      headers: {
+        ...headers,
+        'Cookie': cookies,
+      },
+      credentials: 'omit',
+    });
+  }
+
+  return fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers,
+  });
+}
