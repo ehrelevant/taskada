f7f4ffa38f2ba8a60e7fa74c499250187d2438e7
Ehren Castillo <ehrencastillo@gmail.com>
2025-12-14 03:10:47 +0800

    feat(server): add service-types module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -5,12 +5,14 @@ import { auth } from './auth';
 
 import { DatabaseModule } from './database/database.module';
 import { ProvidersModule } from './providers/providers.module';
+import { ServiceTypesModule } from './service-types/service-types.module';
 
 @Module({
   imports: [
     DatabaseModule,
     AuthModule.forRoot({ auth }),
     ProvidersModule,
+    ServiceTypesModule
   ],
 })
 export class AppModule {}
