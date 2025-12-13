b6dcccf2a8273753da66399bd65a29864b5d342e
Ehren Castillo <ehrencastillo@gmail.com>
2025-12-13 19:05:11 +0800

    feat(provider-app): clean up styling of requests list

--- apps/provider-app/src/screens/home/request/RequestListScreen.tsx
+++ apps/provider-app/src/screens/home/request/RequestListScreen.tsx
@@ -1,5 +1,6 @@
 import { apiFetch } from '@lib/helpers';
 import { Button, Typography } from '@repo/components';
+import { colors } from '@repo/theme';
 import { Image, StyleSheet, View } from 'react-native';
 import { Provider } from '@repo/database';
 import { useEffect, useState } from 'react';
@@ -30,6 +31,10 @@ export function RequestListScreen() {
 
   return (isAccepting) ? (
     <View style={styles.screen}>
+      <View style={styles.requestsListContainer}>
+
+      </View>
+
       <View style={styles.bottomButtonContainer}>
         <Button title="Stop Receiving Requests" variant="outline" onPress={disableRequests} />
       </View>
@@ -53,28 +58,8 @@ export function RequestListScreen() {
 }
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 60,
-  },
-  textContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: '10%'
-  },
-  buttonContainer: {
-    width: '100%',
-    paddingHorizontal: 10,
-    gap: 10,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-
   screen: {
     flex: 1,
-    backgroundColor: 'white',
   },
   center: {
     flex: 1,
@@ -86,10 +71,21 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 100,
   },
+  textContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: '10%'
+  },
   bottomButtonContainer: {
     position: 'absolute',
-    left: 8,
-    right: 8,
-    bottom: 8,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    padding: 8,
+    borderTopColor: colors.border,
+    borderTopWidth: 2
   },
+  requestsListContainer: {
+    flex: 1,
+  }
 });
