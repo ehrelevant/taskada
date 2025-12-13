f8ef43014ecf7c094dc85baf2cbca3ef67ad8d72
Ehren Castillo <ehrencastillo@gmail.com>
2025-12-13 17:23:04 +0800

    refactor(provider-app): ensure that request method cannot be passed in options

--- apps/provider-app/lib/helpers.ts
+++ apps/provider-app/lib/helpers.ts
@@ -3,7 +3,7 @@ import { authClient } from "./authClient";
 
 type RequestMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
 
-export async function apiFetch(endpoint: string, method: RequestMethod = 'GET', options?: RequestInit, authenticated = true) {
+export async function apiFetch(endpoint: string, method: RequestMethod = 'GET', options?: Omit<RequestInit, 'method'>, authenticated = true) {
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
     ...(options?.headers || {}),
