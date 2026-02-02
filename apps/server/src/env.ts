39dec69fa76e144a31a239b1f16542a3d790616a
Ehren Castillo <ehrencastillo@gmail.com>
2026-02-02 11:42:54 +0800

    feat(env): register google maps api key as env variable

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -1,3 +1,5 @@
 import { parse, string } from 'valibot';
 
 export const XENDIT_SECRET_KEY = parse(string(), process.env.XENDIT_SECRET_KEY);
+
+export const GOOGLE_MAPS_API_KEY = parse(string(), process.env.GOOGLE_MAPS_API_KEY);
