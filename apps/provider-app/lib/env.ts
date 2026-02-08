49a12ab04c840e449245a63cc95b6350cb22b2da
Ehren Castillo <ehrencastillo@gmail.com>
2026-02-08 20:39:35 +0800

    feat(provider-app): add google maps api key as an expected env variable

--- apps/provider-app/lib/env.ts
+++ apps/provider-app/lib/env.ts
@@ -1,3 +1,5 @@
 import { parse, string } from 'valibot';
 
 export const API_URL = parse(string(), process.env.EXPO_PUBLIC_API_URL);
+
+export const GOOGLE_MAPS_API_KEY = parse(string(), process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY);
