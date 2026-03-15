f6044e48d447ccef626c4c5dbbcf6fce73025d33
ktrin-u <kdtrinidad2@up.edu.ph>
2026-03-15 22:02:31 +0800

    fix: incorrect example in metadata

--- packages/xendit-payment-engine/src/payments/payment_request/schema/request.ts
+++ packages/xendit-payment-engine/src/payments/payment_request/schema/request.ts
@@ -5,7 +5,7 @@ const payment_request_id_field = z
   .regex(/^[a-zA-Z0-9-]{39}$/)
   .meta({
     description: 'The Payment Request Id',
-    example: ['pr-8877c08a-740d-4153-9816-3d744ed197a5', 'pr-1102feb0-bb79-47ae-9d1e-e69394d3949c'],
+    example: 'pr-8877c08a-740d-4153-9816-3d744ed197a5',
   });
 
 const payment_request_id_object = z.object({
@@ -20,6 +20,6 @@ export const SimulatePaymentRequestSchema = z.object({
   payment_request_id: payment_request_id_field,
   amount: z.number().meta({
     description: 'Amount to simulate',
-    example: [10000.0, 5000.0],
+    example: 100.0,
   }),
 });
