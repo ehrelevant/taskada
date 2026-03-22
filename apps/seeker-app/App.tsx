a67c801a5f1e14d9b8b1239ca6dcb1f70c7edd6c
Ehren Castillo <ehrencastillo@gmail.com>
2026-03-22 19:01:06 +0800

    fix: change react-query imports to @tanstack/react-query

--- apps/seeker-app/App.tsx
+++ apps/seeker-app/App.tsx
@@ -1,6 +1,6 @@
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 import { LoadingProvider, queryClient } from '@repo/shared';
-import { QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { RootNavigator } from '@navigation/RootNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
